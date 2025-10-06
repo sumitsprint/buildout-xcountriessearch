590e0723ed41a729e10742d4c3c4992f5d6970d6
Sumit Kumar. <ncr2099@gmail.com>
2025-10-06 09:04:13 +0530

    xCountries Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState("");
+  
+
+  useEffect(() => {
+    (async () => {
+      try{
+     const res = await axios.get('https://countries-search-data-prod-812920491762.asia-south1.run.app/countries');
+     setCountries(res.data);
+      }
+      catch(err){
+        console.error('unable to fetch:', err);
+      }
+
+    })();
+
+  },[]);
+  const filteredCountries = query.trim() === ""? countries: countries.filter((country) => (country.common.toLowerCase().startsWith(query.toLowerCase())));
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+       <div className='App'>
+        <br/>
+        
+        <div style={{  display:'flex', justifyContent:'center'}}>
+          <input
+          style={{ width: '50%'}}
+          
+          name = "search"
+          type = "text"
+          placeholder='Search for countries...'
+          value = {query}
+          onChange={(e) => (setQuery(e.target.value))}
+
+          />
+        </div>
+        <br/>
+        <div style = {{display:'grid', gridTemplateColumns:'repeat(7, 1fr)', gap: '16px', padding: '20px'}}>
+         {filteredCountries.map((country) => (
+          <div key = {country.common} className="countryCard" style={{textAlign: 'center', border:'1px solid #ccc', borderRadius: '8px', padding: '10px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)'}}>
+            <img
+            src = {country.png}
+            alt = {country.common}
+            width= "100%"
+            height="50%"
+            />
+            <p>{country.common}</p>
+            
+
+
+
+          </div>
+
+
+
+         )
+        
+         )}
+        
+        </div>
+       </div>
   )
 }
 
